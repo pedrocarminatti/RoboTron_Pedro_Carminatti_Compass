3ae693543c259156e8d378cfb62dbc939a0d2ece
Pedro Carminatti <pedrolrc13@gmail.com>
2022-09-29 14:46:34 -0300

    Correction exercise 6

--- Day 7 - Exercises/exercise6.py
+++ Day 7 - Exercises/exercise6.py
@@ -3,6 +3,9 @@ import json
 with open("campeonato.json", encoding='utf-8') as campeonato:
     dados = json.load(campeonato)
 
-print(dados["edicao_atual"]) 
-print(dados["fase_atual"])
-print(dados["rodada_atual"])
+chaves = ['edicao_atual', 'fase_atual',  'rodada_atual']
+
+for chave in chaves:
+    for index, row in dados[chave].items():
+        print(index, ": ", row) 
+        break
