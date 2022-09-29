92972511e86336769cea3bbc0c508c6af889ea44
Pedro Carminatti <pedrolrc13@gmail.com>
2022-09-29 14:34:48 -0300

    Correction exercise 7

--- Day 7 - Exercises/exercise7.py
+++ Day 7 - Exercises/exercise7.py
@@ -3,5 +3,5 @@ import json
 with open("campeonato.json", encoding='utf-8') as campeonato:
     dados = json.load(campeonato)
 
-for i in dados:
-    print(i, ":", dados[i])
+for i in dados.keys():
+    print(i)
